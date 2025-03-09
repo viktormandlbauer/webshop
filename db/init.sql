3b56010fc6404c8a64f3e611fbfafb8c16cdafc5
Viktor Mandlbauer <vikormandlbauer@gmail.com>
2025-03-09 18:06:01 +0100

    Database container for testing authentication

--- db/init.sql
+++ db/init.sql
@@ -1,1 +1,13 @@
 USE webshop;
+
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+INSERT INTO webshop.user
+(id, username, password, created_at)
+VALUES(0, 'admin', 'admin', CURRENT_TIMESTAMP);
